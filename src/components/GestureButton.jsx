d1ff3d321e677dd8f403b441cfff5904f3c2b5aa
Sandipan <sandipandey.12@gmail.com>
2021-04-18 15:44:24 +0530

    working

--- src/components/GestureButton.jsx
+++ src/components/GestureButton.jsx
@@ -1,19 +1,34 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { connect } from "react-redux";
 
 function GestureButton(props) {
   const cursorRef = useRef(null);
+  const [prevInterval, setCurrInterval] = useState(null);
 
   useEffect(() => {
     if (!props.finger_locx) return;
     cursorRef.current.style.display = "flex";
     cursorRef.current.style.left = (window.innerWidth - props.finger_locx[8].x * window.innerWidth) + "px";
     cursorRef.current.style.top = props.finger_locx[8].y * window.innerHeight + "px";
-  }, [props.finger_locx]);
+
+    const i = setTimeout(() => {
+        console.log("HOLA");
+        cursorRef.current.style.display = "none";
+    }, 2000);
+
+    const getPrevInterval = () => prevInterval;
+    if (getPrevInterval()) clearTimeout(getPrevInterval());
+    setCurrInterval(i);
+
+  }, [props.finger_locx, setCurrInterval]);
+
+  let bg = "bg-white";
+  if (props.gesture && props.gesture[0] == "C") bg = "bg-green-500";
+  else if (props.gesture && props.gesture[0] == "G") bg = "bg-blue-500";
 
   return (
   <div 
-    className="absolute w-10 h-10 rounded-full bg-white z-50 hidden items-center justify-center font-bold" 
+    className={`absolute w-12 h-12 text-2xl rounded-full z-50 hidden items-center justify-center font-bold ${bg}`}
     ref={cursorRef}>
       {props.gesture && props.gesture[0]}
   </div>);
